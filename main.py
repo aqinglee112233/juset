3776b78d3cf4a973e6abc57a091f852ea681bf0f
aqinglee112233 <110798972+aqinglee112233@users.noreply.github.com>
2023-05-01 21:38:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import openai
-openai.api_key = "YOUR API KEY"
-prompt = "你会Python开发吗？"
-model = "text-davinci-002"
-response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=60)
-print(response.choices[0].text)
+from flask import Flask, request, jsonify
+
+# 设置 OpenAI API 密钥
+openai.api_key = "YOUR_API_KEY"
+
+# 初始化 Flask 应用程序
+app = Flask(__name__)
+
+# 定义 Flask 路由
+@app.route('/', methods=['POST'])
+def generate_text():
+    # 从 POST 请求中获取请求体
+    data = request.get_json()
+
+    # 获取生成文本的参数
+    prompt = data['prompt']
+    length = data['length']
+
+    # 使用 OpenAI API 生成文本
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=length
+    )
+
+    # 返回生成的文本
+    return jsonify({'text': response['choices'][0]['text']})
+
+# 运行 Flask 应用程序
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
